1407b69a6a9833becf5393bd6b6769ec7b3d7e7f
Jacob Border <jborder@windows.linburg.com>
2017-08-24 10:15:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/pltw/testwow2/QuackQuack.java
@@ -0,0 +1,13 @@
+package com.example.pltw.testwow2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuackQuack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quack_quack);
+    }
+}
